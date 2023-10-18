2c1f40b3171d5dfebaac7519968e4abf4fa0a83c
Sallahtech <donlyrlmrktr@gmail.com>
2023-10-18 23:16:53 +0300

    added pagination and Remove .parcel-cache from version control

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,4 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const API_KEY = '3455bdb0-0819-4c20-925b-41771981a8a1';
 export const TIMEOUT_SECONDS = 10;
+export const RESULTS_PER_PAGE = 12;
--- src/js/controller.js
+++ src/js/controller.js
@@ -4,6 +4,7 @@ import * as model from './model.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
+import paginationView from './views/paginationView.js';
 
 if (module.hot) {
   module.hot.accept();
@@ -13,7 +14,7 @@ const controlRecipes = async function () {
   try {
     const id = window.location.hash.slice(1);
 
-    // if (!id) return;
+    if (!id) return;
 
     // 1) Loading recipe
     recipeView.renderASpinner();
@@ -38,15 +39,25 @@ const controlSearchResults = async function () {
 
     await model.loadSearchResults(query);
 
-    resultsView.render(model.state.search.results);
+    resultsView.render(model.getSearchResultsPage());
+
+    // 3) Render initial pagination buttons
+    paginationView.render(model.state.search);
   } catch (error) {
     console.error(`${error} 💥💥💥💥💥`);
   }
 };
 
+const controlPagination = function (goToPage) {
+  resultsView.render(model.getSearchResultsPage(goToPage));
+
+  paginationView.render(model.state.search);
+};
+
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
   searchView.addHandlerSearch(controlSearchResults);
+  paginationView.addHandlerClick(controlPagination);
 };
 
 init();
