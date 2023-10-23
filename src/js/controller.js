32461b689ae16aa02eaddcb4f1b91f8e81e0d617
Sallahtech <donlyrlmrktr@gmail.com>
2023-10-23 23:08:42 +0300

    implemeted the add new recipe view and controller

--- src/js/controller.js
+++ src/js/controller.js
@@ -6,6 +6,7 @@ import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
 import paginationView from './views/paginationView.js';
 import bookmarksView from './views/bookmarksView.js';
+import addRecipeView from './views/addRecipeView.js';
 
 // if (module.hot) {
 //   module.hot.accept();
@@ -86,6 +87,12 @@ const controlBookmarks = function () {
   bookmarksView.render(model.state.bookmarks);
 };
 
+const controlAddRecipe = async function (newRecipe) {
+  console.log(newRecipe);
+
+  // upload the new recipe data
+};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -93,6 +100,7 @@ const init = function () {
   recipeView.addHandlerAddBookmark(controlAddNewBookmark);
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
+  addRecipeView.addHandlerUpload(controlAddRecipe);
 };
 
 init();
